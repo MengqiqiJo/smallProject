33fdbb2e82359e9de72d73f90ed1697a654809be
MengqiqiJo <763786730@qq.com>
2017-10-23 10:52:08 -0400

    improve checkbox

--- empty-setup/vueCheckBox.php
+++ empty-setup/vueCheckBox.php
@@ -1,9 +1,13 @@
 <?php
 
-echo "<link rel='stylesheet prefetch' href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,400italic'>";
-echo "<link rel='stylesheet prefetch' href='https://fonts.googleapis.com/icon?family=Material+Icons'>";
+echo '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">';
+echo '<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>';
+echo '<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>';
+echo '<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>';
+
 echo "<link rel='stylesheet prefetch' href='https://unpkg.com/vue-material@latest/dist/vue-material.css'>";
 echo "<link rel='stylesheet' href='css/style.css'>";
+echo "<link rel='stylesheet' href='../plugin.css'>";
 
 /**
 *
@@ -13,50 +17,50 @@ class popUpWithCheckBox {
   function popUpWithCH() {
     $output = '';
     $output .= '<div id="app">';
+
       $output .= '<md-toolbar>';
-      $output .= '<h1 class="md-title">My Title</h1>';
+        $output .= '<h1 class="md-title"></h1>';
       $output .= '</md-toolbar>';
 
-      $output .= '<div class="main-content">';
-        $output .= '<div>';
+      $output .= '<md-layout md-gutter>';
+        $output .= '<md-layout md-flex="10" md-flex-offset="40">';
           $output .= '<md-radio v-model="radio1" name="my-test-group1" md-value="Respirologist">Respirologist</md-radio>';
           $output .= '<md-radio v-model="radio1" name="my-test-group1" md-value="Pathologist">Pathologist</md-radio>';
-        $output .= '</div>';
-        $output .= '<div v-if="radio1==\'Respirologist\' || radio1==\'Pathologist\'">';
-        $output .= '</div>';
-        $output .= '<div v-else>';
-        $output .= '<span>(* Please select role)</span>';
-        $output .= '<span>{{radio1}}</span>';
+        $output .= '</md-layout>';
 
-        $output .= '</div>';
-        $output .= '<div>';
-          $output .= '<p>Terms: I have discussed with my patient that I will be discussing their case in the MILDDER platform.</p>';
-        $output .= '</div>';
-
-        $output .= '<div>';
+        $output .= '<md-layout md-flex="40" md-flex-offset="30">';
+          $output .= '<p class="font-bold font-size-14"><span class="font-bold font-size-18">Terms:</span> I have discussed with my patient that I will be discussing their case in the MILDDER platform.</p>';
+        $output .= '</md-layout>';
+        $output .= '<md-layout md-flex="60" md-flex-offset="40">';
           $output .= '<md-checkbox  id="my-test5" name="my-test5" v-model="checkbox1" class="md-primary" required>I Agree</md-checkbox>';
-          $output .= '<span class="md-error">Validation message</span>';
-          $output .= '<md-tooltip md-delay="400" md-direction="left-bottom">My tooltip</md-tooltip>';
-          $output .= '</md-avatar>';
-        $output .= '</div>';
+        $output .= '</md-layout>';
 
-        $output .= '<div v-if="!checkbox1">';
-          $output .= '<span>(* Please choose "I Agree")</span>';
-          $output .= '<span>{{checkbox1}}</span>';
-        $output .= '</div>';
+        $output .= '<md-layout md-flex="60" md-flex-offset="40">';
+          $output .= '<md-layout v-if="radio1==\'Respirologist\' || radio1==\'Pathologist\'">';
+          $output .= '</md-layout>';
+          $output .= '<md-layout v-else>';
+           $output .= '<span class="color-f00000">(* Please select role)</span>';
+          $output .= '</md-layout>';
+        $output .= '</md-layout>';
+        $output .= '<md-layout md-flex="60" md-flex-offset="40">';
+          $output .= '<md-layout v-if="!checkbox1">';
+            $output .= '<span class="color-f00000">(* Please choose "I Agree")</span>';
+          $output .= '</md-layout>';
+        $output .= '</md-layout>';
 
-        $output .= '<div v-if="(radio1==\'Respirologist\' || radio1==\'Pathologist\') && checkbox1">';
-          $output .= '<div>';
-           $output .= '<md-button href="/mildder9" class="md-raised md-primary">submit</md-button>';
-          $output .= '</div>';
-        $output .= '</div>';
-
-        $output .= '<div v-else>';
-          $output .= '<md-button href="" class="md-raised md-primary">submit</md-button>';
-        $output .= '</div>';
-
-      $output .= '</div>';
+        $output .= '<md-layout md-flex="60" md-flex-offset="40">';
+          $output .= '<md-layout v-if="(radio1==\'Respirologist\' || radio1==\'Pathologist\') && checkbox1">';
+            $output .= '<md-layout>';
+              $output .= '<md-button href="/mildder9" class="md-raised md-primary">submit</md-button>';
+             $output .= '</md-layout>';
+          $output .= '</md-layout>';
+          $output .= '<md-layout v-else>';
+            $output .= '<md-button href="" class="md-raised md-primary">submit</md-button>';
+          $output .= '</md-layout>';
+        $output .= '</md-layout>';
+      $output .= '</md-layout>';
     $output .= '</div>';
+
     $output .= '<script src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.1.6/vue.min.js"></script>';
     $output .= '<script src="https://unpkg.com/vue-material@latest"></script>';
     $output .= '<script src="js/index.js"></script>';
--- vueMaterial-v2Checkbox copy/vuejs.js
+++ vueMaterial-v2Checkbox copy/vuejs.js
@@ -1,5 +1,6 @@
-var Vue = require('vue')
-var VueMaterial = require('vue-material')
+import 'vue-material/dist/vue-material.css';
+var Vue = require('vue');
+var VueMaterial = require('vue-material');
 
 Vue.use(VueMaterial);
 new Vue({
