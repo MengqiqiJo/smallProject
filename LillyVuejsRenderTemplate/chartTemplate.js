00cd74338d244820a2cdc67e58354f571fd4b3dd
MengqiqiJo <763786730@qq.com>
2017-11-16 13:26:59 -0500

    change json file

--- LillyVuejsRenderTemplate/chartTemplate.js
+++ LillyVuejsRenderTemplate/chartTemplate.js
@@ -26,7 +26,7 @@ Vue.component('vue-chartjs-pie', {
     },
   },
   template:`
-    <div>
+    <div class="vue-chart-block">
       <div class="col-sm-12 col-md-6 col-lg-6">
         <div class="margin-left-20 margin-top-16">
           <div class="border-1-e7e7e7">
@@ -44,38 +44,38 @@ Vue.component('vue-chartjs-pie', {
  * @
  */
 var app = new Vue({
-    el: "#page-wrapper",
-    data: {
-      blockData: "",
-      pieChartData: "",
-      pieChartOptions: "",
-      barChartData: "",
-      barChartOptions: "",
-      pieChartType: "pie",
-      doughnutChartType: "doughnut",
-      lineChartType: "line",
-      barChartType: "bar",
-    },
-    created: function () {
-       var self = this;
-       axios.get('jsonData.json').then((response) => {
-         console.log("3333");
-         self.pieChartData = response.data.contentSection[0].middle.middleMiddle.middleMiddleMiddle.chartData;
-         self.pieChartOptions = response.data.contentSection[0].middle.middleMiddle.middleMiddleMiddle.chartOptions;
-         self.barChartData = response.data.contentSection[1].middle.middleMiddle.middleMiddleMiddle.chartData;
-         self.barChartOptions = response.data.contentSection[1].middle.middleMiddle.middleMiddleMiddle.chartOptions;
-         console.log(self.pieChartOptions);
-         console.log(self.barChartOptions);
-       })
-       .catch((error) => {
-         console.log(error);
-       });
-    },
-    template:`
-      <div>
-        <vue-chartjs-pie v-bind:pieChartData="pieChartData" v-bind:pieChartOptions="pieChartOptions" v-bind:chartType="pieChartType"></vue-chartjs-pie>
-        <vue-chartjs-pie v-bind:pieChartData="pieChartData" v-bind:pieChartOptions="pieChartOptions" v-bind:chartType="doughnutChartType"></vue-chartjs-pie>
-        <vue-chartjs-pie v-bind:pieChartData="barChartData" v-bind:pieChartOptions="barChartOptions" v-bind:chartType="barChartType"></vue-chartjs-pie>
-        <vue-chartjs-pie v-bind:pieChartData="barChartData" v-bind:pieChartOptions="barChartOptions" v-bind:chartType="lineChartType"></vue-chartjs-pie>
-      </div>`
-  });
+  el: "#page-wrapper",
+  data: {
+    blockData: "",
+    pieChartData: "",
+    pieChartOptions: "",
+    barChartData: "",
+    barChartOptions: "",
+    pieChartType: "pie",
+    doughnutChartType: "doughnut",
+    lineChartType: "line",
+    barChartType: "bar",
+  },
+  created: function () {
+     var self = this;
+     axios.get('jsonData.json').then((response) => {
+       console.log("3333");
+       self.pieChartData = response.data.contentSection[0].middle.middleMiddle.middleMiddleMiddle.chartData;
+       self.pieChartOptions = response.data.contentSection[0].middle.middleMiddle.middleMiddleMiddle.chartOptions;
+       self.barChartData = response.data.contentSection[1].middle.middleMiddle.middleMiddleMiddle.chartData;
+       self.barChartOptions = response.data.contentSection[1].middle.middleMiddle.middleMiddleMiddle.chartOptions;
+       console.log(self.pieChartOptions);
+       console.log(self.barChartOptions);
+     })
+     .catch((error) => {
+       console.log(error);
+     });
+  },
+  template:`
+    <div>
+      <vue-chartjs-pie v-bind:pieChartData="pieChartData" v-bind:pieChartOptions="pieChartOptions" v-bind:chartType="pieChartType"></vue-chartjs-pie>
+      <vue-chartjs-pie v-bind:pieChartData="pieChartData" v-bind:pieChartOptions="pieChartOptions" v-bind:chartType="doughnutChartType"></vue-chartjs-pie>
+      <vue-chartjs-pie v-bind:pieChartData="barChartData" v-bind:pieChartOptions="barChartOptions" v-bind:chartType="barChartType"></vue-chartjs-pie>
+      <vue-chartjs-pie v-bind:pieChartData="barChartData" v-bind:pieChartOptions="barChartOptions" v-bind:chartType="lineChartType"></vue-chartjs-pie>
+    </div>`
+});
