53482634243fbf9a55857af7340061507f85d20a
MengqiqiJo <763786730@qq.com>
2017-11-20 10:47:54 -0500

    clean parameter name

--- LillyVuejsRenderTemplate/chartTemplate.js
+++ LillyVuejsRenderTemplate/chartTemplate.js
@@ -10,7 +10,7 @@ Vue.component('vue-chartjs-top', {
  * @
  */
 Vue.component('vue-chartjs-pie', {
-  props: ['pieChartData', 'pieChartOptions', 'chartTop', 'chartType', 'chartTopClass'],
+  props: ['chartData', 'chartOptions', 'chartTop', 'chartType', 'chartTopClass'],
   methods: {
     drawChart: function(canvas, type, chartData, chartOptions) {
       console.log("2222");
@@ -22,8 +22,8 @@ Vue.component('vue-chartjs-pie', {
     }
   },
   watch: {
-    pieChartData: function () {
-      this.drawChart(this.$refs.canvas, this.chartType, this.pieChartData, this.pieChartOptions);
+    chartData: function () {
+      this.drawChart(this.$refs.canvas, this.chartType, this.chartData, this.chartOptions);
     },
   },
   template:`
@@ -48,21 +48,14 @@ var app = new Vue({
   el: "#page-wrapper",
   data: {
     blockData: "",
-    pieChartData: [],
-    pieChartOptions: [],
-    barChartData: "",
-    barChartOptions: "",
+    chartData: [],
+    chartOptions: [],
     pieChartType: "pie",
     doughnutChartType: "doughnut",
     lineChartType: "line",
     barChartType: "bar",
-    pieChartTop: [],
-    barChartTop: "",
-    pieChartTopClass: [],
-    barChartTopClass: "",
-    data: "",
-    option: "",
-    top: "",
+    chartTop: [],
+    chartTopClass: [],
     topClass: ""
   },
   created: function () {
@@ -72,13 +65,13 @@ var app = new Vue({
       countChart = response.data.contentSection.length;
       // console.log(countChart);
       for(var i=0; i<countChart; i++) {
-        self.pieChartData[i] = response.data.contentSection[i].middle.middleMiddle.middleMiddleMiddle.chartData;
-        self.pieChartOptions[i] = response.data.contentSection[i].middle.middleMiddle.middleMiddleMiddle.chartOptions;
-        self.pieChartTop[i] = response.data.contentSection[i].top.value;
-        self.pieChartTopClass[i] = response.data.contentSection[i].top.class;
+        self.chartData[i] = response.data.contentSection[i].middle.middleMiddle.middleMiddleMiddle.chartData;
+        self.chartOptions[i] = response.data.contentSection[i].middle.middleMiddle.middleMiddleMiddle.chartOptions;
+        self.chartTop[i] = response.data.contentSection[i].top.value;
+        self.chartTopClass[i] = response.data.contentSection[i].top.class;
       }
 
-      self.topClass = self.pieChartTopClass[0];
+      self.topClass = self.chartTopClass[0];
 
       })
       .catch((error) => {
@@ -87,8 +80,8 @@ var app = new Vue({
   },
   template:`
     <div>
-      <vue-chartjs-pie v-bind:pieChartData=pieChartData[0] v-bind:pieChartOptions=pieChartOptions[0] v-bind:chartTop=pieChartTop[0] v-bind:chartType="doughnutChartType" v-bind:chartTopClass=pieChartTopClass[0]></vue-chartjs-pie>
-      <vue-chartjs-pie v-bind:pieChartData=pieChartData[0] v-bind:pieChartOptions=pieChartOptions[0] v-bind:chartTop=pieChartTop[0] v-bind:chartType="pieChartType" v-bind:chartTopClass=pieChartTopClass[0]></vue-chartjs-pie>
-      <vue-chartjs-pie v-bind:pieChartData=pieChartData[1] v-bind:pieChartOptions=pieChartOptions[1] v-bind:chartTop=pieChartTop[1] v-bind:chartType="barChartType" v-bind:chartTopClass="topClass"></vue-chartjs-pie>
+      <vue-chartjs-pie v-bind:chartData=chartData[0] v-bind:chartOptions=chartOptions[0] v-bind:chartTop=chartTop[0] v-bind:chartType="doughnutChartType" v-bind:chartTopClass=chartTopClass[0]></vue-chartjs-pie>
+      <vue-chartjs-pie v-bind:chartData=chartData[0] v-bind:chartOptions=chartOptions[0] v-bind:chartTop=chartTop[0] v-bind:chartType="pieChartType" v-bind:chartTopClass=chartTopClass[0]></vue-chartjs-pie>
+      <vue-chartjs-pie v-bind:chartData=chartData[1] v-bind:chartOptions=chartOptions[1] v-bind:chartTop=chartTop[1] v-bind:chartType="barChartType" v-bind:chartTopClass="topClass"></vue-chartjs-pie>
     </div>`
 });
