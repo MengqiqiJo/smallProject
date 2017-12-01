00b39d9521178f93331d7c5e0b87645436ccab27
MengqiqiJo <763786730@qq.com>
2017-12-01 10:45:08 -0500

    dev multi tabs

--- LillyVuejsRenderTemplate/chartTemplate.js
+++ LillyVuejsRenderTemplate/chartTemplate.js
@@ -6,6 +6,9 @@ Vue.component('vue-chartjs-top', {
   template: '<div v-if="chartTop != \'\' " class="bg-0082ba max-height-32 color-fff padding-10" v-html="chartTop"></div>'
 })
 
+/**
+ * @
+ */
 Vue.component('vue-chartjs-bottom', {
   props: ['chartBottomValue'],
   template: '<div v-if="chartBottomValue != \'\' " class="border-1-e7e7e7" v-html="chartBottomValue"></div>'
@@ -33,10 +36,9 @@ Vue.component('vue-chartjs-pie', {
   },
   template:`
     <div class="vue-chart-block">
-      <div class="col-md-12" v-bind:class="chartTopClass">
+      <div  v-bind:class="chartTopClass">
         <div class="margin-left-20 margin-top-16">
           <div class="border-1-e7e7e7">
-            <vue-chartjs-top v-bind:chartTop="chartTop"></vue-chartjs-top>
             <div class="row">
               <div v-bind:class="chartMiddleMiddleClass">
                 <div class="margin-50 max-height-480">
@@ -48,6 +50,58 @@ Vue.component('vue-chartjs-pie', {
               </div>
             </div>
             <vue-chartjs-bottom v-bind:chartBottomValue="chartBottomValue"></vue-chartjs-bottom>
+          </div>
+        </div>
+      </div>
+    </div>`
+});
+
+/**
+ * @
+ */
+Vue.component('vue-chartjs-multiTabs', {
+  props: ['chartData', 'chartOptions', 'chartTop', 'chartType', 'chartTopClass', 'chartMiddleMiddleClass', 'chartMiddleRightClass', 'chartBottomValue'],
+  // methods: {
+  //   drawChart: function(canvas, type, chartData, chartOptions) {
+  //     console.log("tabs");
+  //     let chart = new Chart(canvas, {
+  //       type: type,
+  //       data: chartData,
+  //       options: chartOptions,
+  //     })
+  //   }
+  // },
+  // watch: {
+  //   chartData: function () {
+  //     this.drawChart(this.$refs.canvas, this.chartType, this.chartData, this.chartOptions);
+  //   },
+  // },
+  template:`
+    <div class="container col-md-6">
+      <div class="border-1-e7e7e7">
+        <vue-chartjs-top v-bind:chartTop="chartTop"></vue-chartjs-top>
+        <div class="row">
+          <div class="col-md-12">
+            <ul class="nav nav-tabs">
+              <li><a data-toggle="tab" href="#chart1">chart 1</a></li>
+              <li><a data-toggle="tab" href="#chart2">chart 2</a></li>
+              <li><a data-toggle="tab" href="#chart3">chart 3</a></li>
+              <li><a data-toggle="tab" href="#chart4">chart 4</a></li>
+            </ul>
+            <div class="tab-content col-md-12">
+              <div id="chart1" class="tab-pane fade">
+                <vue-chartjs-pie v-bind:chartData=chartData v-bind:chartOptions=chartOptions v-bind:chartTop=chartTop v-bind:chartType=chartType v-bind:chartTopClass=chartTopClass v-bind:chartMiddleMiddleClass=chartMiddleMiddleClass v-bind:chartMiddleRightClass=chartMiddleRightClass v-bind:chartBottomValue=chartBottomValue></vue-chartjs-pie>
+              </div>
+              <div id="chart2" class="tab-pane fade">
+                <h3>Menu 1</h3>
+              </div>
+              <div id="chart3" class="tab-pane fade">
+                <h3>Menu 2</h3>
+              </div>
+              <div id="chart4" class="tab-pane fade">
+                <vue-chartjs-pie v-bind:chartData=chartData v-bind:chartOptions=chartOptions v-bind:chartTop=chartTop v-bind:chartType=chartType v-bind:chartTopClass=chartTopClass v-bind:chartMiddleMiddleClass=chartMiddleMiddleClass v-bind:chartMiddleRightClass=chartMiddleRightClass v-bind:chartBottomValue=chartBottomValue></vue-chartjs-pie>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -157,7 +211,7 @@ var app = new Vue({
   },
   template:`
     <div>
-      <vue-chartjs-pie v-bind:chartData=chart[0].chartData v-bind:chartOptions=chart[0].chartOptions v-bind:chartTop=chart[0].chartTop v-bind:chartType="doughnutChartType" v-bind:chartTopClass=chart[0].chartTopClass v-bind:chartMiddleMiddleClass=chart[0].chartMiddleMiddleClass v-bind:chartMiddleRightClass=chart[0].chartMiddleRightClass v-bind:chartBottomValue=chart[0].chartBottomValue></vue-chartjs-pie>
+      <vue-chartjs-multiTabs v-bind:chartData=chart[2].chartData v-bind:chartOptions=chart[2].chartOptions v-bind:chartTop=chart[2].chartTop v-bind:chartType="doughnutChartType" v-bind:chartTopClass=chart[2].chartTopClass v-bind:chartMiddleMiddleClass=chart[2].chartMiddleMiddleClass v-bind:chartMiddleRightClass=chart[2].chartMiddleRightClass v-bind:chartBottomValue=chart[2].chartBottomValue></vue-chartjs-multiTabs>
       <vue-chartjs-pie v-bind:chartData=chart[0].chartData v-bind:chartOptions=chart[0].chartOptions v-bind:chartTop=chart[0].chartTop v-bind:chartType="pieChartType" v-bind:chartTopClass=chart[0].chartTopClass v-bind:chartMiddleMiddleClass=chart[0].chartMiddleMiddleClass v-bind:chartMiddleRightClass=chart[0].chartMiddleRightClass v-bind:chartBottomValue=chart[0].chartBottomValue></vue-chartjs-pie>
       <vue-chartjs-pie v-bind:chartData=chart[1].chartData v-bind:chartOptions=chart[1].chartOptions v-bind:chartTop=chart[1].chartTop v-bind:chartType="barChartType" v-bind:chartTopClass=chart[1].chartTopClass v-bind:chartMiddleMiddleClass=chart[1].chartMiddleMiddleClass v-bind:chartMiddleRightClass=chart[1].chartMiddleRightClass v-bind:chartBottomValue=chart[1].chartBottomValue></vue-chartjs-pie>
     </div>`
--- LillyVuejsRenderTemplate/lillyVueTemplate.php
+++ LillyVuejsRenderTemplate/lillyVueTemplate.php
@@ -3,7 +3,8 @@
 
 echo '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">';
 echo '<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>';
-echo "<link rel='stylesheet' href='../plugin.css'>";
+echo '<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>';
+echo '<link rel="stylesheet" href="../plugin.css">';
 
 /**
  * @file
@@ -22,7 +23,7 @@ class dashpageBlockGenerator {
     // $output .= '<vue-chartjs-bar></vue-chartjs-bar>';
     // $output .= '<vue-chartjs-line></vue-chartjs-line>';
       // $output .= '<p>{{blockData}}</p>';
-      $output .= '<p>{{pieChartData}}</p>';
+      // $output .= '<p>{{pieChartData}}</p>';
       // $output .= '<p>{{barChartData}}</p>';
     $output .= '</div>';
 
