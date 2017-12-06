0ae26aa888dd1ede75f13a42013ec62a4cf8ae94
MengqiqiJo <763786730@qq.com>
2017-12-06 13:26:23 -0500

    cc

--- outlook-calendar/index.php
+++ outlook-calendar/index.php
@@ -9,7 +9,6 @@ class clickToOutlookCalendar {
     $output .= '</div>';
 
     return $output;
-
   }
 
 }
--- outlook-calendar/phptoics/index.php
+++ outlook-calendar/phptoics/index.php
@@ -18,22 +18,6 @@ class clickToOutlookCalendar {
     return $output;
   }
 
-  public function linkToOutlookCalendar($date_start) {
-    $output = '';
-    $date_start = "2017-1-16 9:00AM";
-
-
-    $output .= '<form method="post" action="downloadics.php">';
-      $output .= '<input type="hidden" name="date_start" value=>';
-      $output .= '<input type="hidden" name="date_end" value="2017-1-16 10:00AM">';
-      $output .= '<input type="hidden" name="location" value="123 Fake St, New York, NY">';
-      $output .= '<input type="hidden" name="description" value="This is my description">';
-      $output .= '<input type="hidden" name="summary" value="This is my summary">';
-      $output .= '<input type="hidden" name="url" value="http://example.com">';
-      $output .= '<input type="submit" value="Add to Calendar">';
-    $output .= '</form>';
-    return $output;
-  }
 }
 
 $addLinkToButton = new clickToOutlookCalendar();
