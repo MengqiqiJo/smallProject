81981789480576972c1694b7bc95892d5d17788e
MengqiqiJo <763786730@qq.com>
2017-12-06 11:50:53 -0500

    dev calendar

--- outlook-calendar/getics.php
+++ outlook-calendar/getics.php
@@ -3,13 +3,13 @@
 $UID = 'http://www.icalmaker.com/event/85709fce-2655-40bc-b90f-29efe88cb8f9';
 $DTSTART = '20171216T050000Z';
 $DTEND = '20171220T090000Z';
-$SUMMARY = 'hello world';
+$SUMMARY = 'hello worldsssss';
 
 header("Content-Type: text/Calendar");
 header("Content-Disposition: inline");
 echo "BEGIN:VCALENDAR
 VERSION:2.0
-PRODID:-//hacksw/handcal//NONSGML v1.0//EN
+PRODID:http://www.icalmaker.com
 BEGIN:VEVENT
 UID:$UID
 DTSTAMP:20171206T164709Z
