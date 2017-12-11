40c45301d86a041b6d07c1be23643b691c6bc14f
MengqiqiJo <763786730@qq.com>
2017-12-11 12:19:24 -0500

    get data from json

--- vuetable-2 options/js/index.js
+++ vuetable-2 options/js/index.js
@@ -5,16 +5,23 @@ Vue.use(VueTables.client, {
 new Vue({
   el: "#vue-tables-options",
   data: {
-    tableDetail: ''
+    columns: [],
+    data: [],
+    options: {sortable: ['option','type']}
   },
   created: function () {
     var self = this;
     axios.get('tableData.json').then((response) => {
-        self.tableDetail = response.data;
-        console.log(response.data);
-      })
-      .catch((error) => {
+      self.columns = response.data.columns;
+      self.data = response.data.data;
+      // self.options = response.data.options;
+      console.log(self.columns);
+      console.log(self.data);
+      console.log(self.options);
+    })
+    .catch((error) => {
       console.log(error);
     });
   }
+
 })
