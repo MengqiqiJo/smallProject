b8c9aa6a54d5931de010a5d2e498b116ea2bda16
MengqiqiJo <763786730@qq.com>
2017-12-19 12:20:45 -0500

    advance tab

--- bootstrapPopUp/phizerModalAndTab.php
+++ bootstrapPopUp/phizerModalAndTab.php
@@ -60,7 +60,7 @@
       $output .= '<div>';
         $output .= '<ul class="nav nav-tabs">';
           $output .= '<li><a class="color-fff" data-toggle="tab" href="#tab1">Home</a></li>';
-          $output .= '<li><a class="color-fff" data-toggle="tab" href="#menu1">Menu 1</a></li>';
+          $output .= '<li><a class="color-fff" data-toggle="tab" href="#tab2">Menu 1</a></li>';
         $output .= '</ul>';
       $output .= '</div>';
 
@@ -83,9 +83,23 @@
             }
           $output .= '</div>';
         $output .= '</div>';
-        $output .= '<div id="menu1" class="tab-pane fade">';
-          $output .= '<h3>Menu 1</h3>';
-          $output .= '<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>';
+        $output .= '<div id="tab2" class="tab-pane fade">';
+          $output .= '<div class="row bg-ffffff padding-top-20 padding-bottom-20">';
+            $output .= '<div class="col-md-3">';
+              $output .= '<div class="text-align-center">';
+                $output .= '<p>' . $tabVaule[0]["tabNumber"] . '</p>';
+                $output .= '<p class="font-size-10">' . $tabVaule[0]["tabContent"] . '</p>';
+              $output .= '</div>';
+            $output .= '</div>';
+            for ($i = 1; $i < $countTabContent; $i++) {
+              $output .= '<div class="col-md-2">';
+                $output .= '<div class="text-align-center border-left-2-e2e2e2">';
+                  $output .= '<p>' . $tabVaule[$i]["tabNumber"] . '</p>';
+                  $output .= '<p class="font-size-10">' . $tabVaule[$i]["tabContent"] . '</p>';
+                $output .= '</div>';
+              $output .= '</div>';
+            }
+          $output .= '</div>';
         $output .= '</div>';
       $output .= '</div>';
 
@@ -128,8 +142,28 @@
         $output .= '</div>';
 
         $output .= '<div id="tab22" class="tab-pane fade">';
-          $output .= '<h3>Menu 1</h3>';
-          $output .= '<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>';
+          $output .= '<div class="row bg-ffffff padding-top-20 padding-bottom-20">';
+            $output .= '<table class="table">';
+                $output .= '<thead>';
+                  $output .= '<tr>';
+                    $output .= '<th class="font-size-12">' . $tabVauleSecond[0]["tabContentOne"] . '</th>';
+                    $output .= '<th class="font-size-12">' . $tabVauleSecond[0]["tabContentTwo"] . '</th>';
+                    $output .= '<th class="font-size-12">' . $tabVauleSecond[0]["tabContentThree"] . '</th>';
+                    $output .= '<th class="font-size-12">' . $tabVauleSecond[0]["tabContentFour"] . '</th>';
+                  $output .= '</tr>';
+                $output .= '</thead>';
+                $output .= '<tbody>';
+                for ($i = 1; $i < $countTable; $i++) {
+                  $output .= '<tr>';
+                    $output .= '<th class="font-size-12 font-normal">' . $tabVauleSecond[$i]["tabContentOne"] . '</th>';
+                    $output .= '<th class="font-size-12 font-normal">' . $tabVauleSecond[$i]["tabContentTwo"] . '</th>';
+                    $output .= '<th class="font-size-12 font-normal">' . $tabVauleSecond[$i]["tabContentThree"] . '</th>';
+                    $output .= '<th class="font-size-12 font-normal">' . $tabVauleSecond[$i]["tabContentFour"] . '</th>';
+                  $output .= '</tr>';
+                }
+                $output .= '</tbody>';
+              $output .= '</table>';
+          $output .= '</div>';
         $output .= '</div>';
 
       $output .= '</div>';
