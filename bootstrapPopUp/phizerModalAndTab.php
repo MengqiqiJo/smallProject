00be7137d1afd920abfa31e8433ec404c50a282e
MengqiqiJo <763786730@qq.com>
2017-12-19 11:28:30 -0500

    put tab content in an array

--- bootstrapPopUp/phizerModalAndTab.php
+++ bootstrapPopUp/phizerModalAndTab.php
@@ -40,61 +40,10 @@
               $output .= '<div class="modal-header bg-673ab7">';
                 $output .= '<button type="button" class="close" data-dismiss="modal">&times;</button>';
                 $output .= '<h4 class="modal-title color-fff text-align-center">Modal Header</h4>';
-
-                // $output .= '<ul class="nav nav-tabs">';
-                //   $output .= '<li class="active"><a data-toggle="tab" href="#home">Home</a></li>';
-                //   $output .= '<li><a data-toggle="tab" href="#menu1">Menu 1</a></li>';
-                //   $output .= '<li><a data-toggle="tab" href="#menu2">Menu 2</a></li>';
-                //   $output .= '<li><a data-toggle="tab" href="#menu3">Menu 3</a></li>';
-                // $output .= '</ul>';
-
-                // $output .= '<div class="tab-content">';
-                //   $output .= '<div id="home" class="tab-pane fade in active">';
-                //     $output .= '<h3>HOME</h3>';
-                //     $output .= '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>';
-                //   $output .= '</div>';
-                //   $output .= '<div id="menu1" class="tab-pane fade">';
-                //     $output .= '<h3>Menu 1</h3>';
-                //     $output .= '<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>';
-                //   $output .= '</div>';
-                //   $output .= '<div id="menu2" class="tab-pane fade">';
-                //     $output .= '<h3>Menu 2</h3>';
-                //     $output .= '<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>';
-                //   $output .= '</div>';
-                //   $output .= '<div id="menu3" class="tab-pane fade">';
-                //     $output .= '<h3>Menu 3</h3>';
-                //     $output .= '<p>Eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>';
-                //   $output .= '</div>';
-                // $output .= '</div>';
                 $output .= $this->getTabContent($firstTabValue);
               $output .= '</div>';
 
               $output .= '<div class="modal-body bg-673ab7">';
-
-                // $output .= '<ul class="nav nav-tabs">';
-                //   $output .= '<li class="active"><a data-toggle="tab" href="#home">Home</a></li>';
-                //   $output .= '<li><a data-toggle="tab" href="#menu4">Menu 1</a></li>';
-                //   $output .= '<li><a data-toggle="tab" href="#menu5">Menu 2</a></li>';
-                //   $output .= '<li><a data-toggle="tab" href="#menu6">Menu 3</a></li>';
-                // $output .= '</ul>';
-                // $output .= '<div class="tab-content">';
-                //   $output .= '<div id="home" class="tab-pane fade in active">';
-                //     $output .= '<h3>HOME</h3>';
-                //     $output .= '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>';
-                //   $output .= '</div>';
-                //   $output .= '<div id="menu4" class="tab-pane fade">';
-                //     $output .= '<h3>Menu 1</h3>';
-                //     $output .= '<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>';
-                //   $output .= '</div>';
-                //   $output .= '<div id="menu5" class="tab-pane fade">';
-                //     $output .= '<h3>Menu 2</h3>';
-                //     $output .= '<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>';
-                //   $output .= '</div>';
-                //   $output .= '<div id="menu6" class="tab-pane fade">';
-                //     $output .= '<h3>Menu 3</h3>';
-                //     $output .= '<p>Eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>';
-                //   $output .= '</div>';
-                // $output .= '</div>';
               $output .= $this->getTabTable($secondTabValue);
               $output .= '</div>';
             $output .= '</div>';
@@ -106,6 +55,7 @@
     }
 
     public function getTabContent($tabVaule) {
+      $countTabContent = count($tabVaule);
       $output = '';
       $output .= '<ul class="nav nav-tabs">';
         $output .= '<li><a class="color-fff" data-toggle="tab" href="#tab1">Home</a></li>';
@@ -115,38 +65,14 @@
       $output .= '<div class="tab-content bg-ffffff padding-50">';
         $output .= '<div id="tab1" class="tab-pane fade in active">';
           $output .= '<div class="row margin-top-6">';
-
-            $output .= '<div class="col-md-3">';
-              $output .= '<div class="text-align-center">';
-                $output .= '<p>' . $tabVaule[0]["tabNumber"] . '</p>';
-                $output .= '<p class="font-size-12">' . $tabVaule[0]["tabContent"] . '</p>';
+            for ($i = 0; $i < $countTabContent; $i++) {
+              $output .= '<div class="col-md-2">';
+                $output .= '<div class="text-align-center">';
+                  $output .= '<p>' . $tabVaule[$i]["tabNumber"] . '</p>';
+                  $output .= '<p class="font-size-12">' . $tabVaule[$i]["tabContent"] . '</p>';
+                $output .= '</div>';
               $output .= '</div>';
-            $output .= '</div>';
-            $output .= '<div class="col-md-3">';
-              $output .= '<div class="border-right-2-e2e2e2 border-left-2-e2e2e2 text-align-center">';
-                $output .= '<p>' . $tabVaule[1]["tabNumber"] . '</p>';
-                $output .= '<p class="font-size-12">' . $tabVaule[1]["tabContent"] . '</p>';
-              $output .= '</div>';
-            $output .= '</div>';
-            $output .= '<div class="col-md-2">';
-              $output .= '<div class="text-align-center">';
-                $output .= '<p>' . $tabVaule[2]["tabNumber"] . '</p>';
-                $output .= '<p class="font-size-12">' . $tabVaule[2]["tabContent"] . '</p>';
-              $output .= '</div>';
-            $output .= '</div>';
-            $output .= '<div class="col-md-2">';
-              $output .= '<div class="border-right-2-e2e2e2 border-left-2-e2e2e2 text-align-center">';
-                $output .= '<p>' . $tabVaule[3]["tabNumber"] . '</p>';
-                $output .= '<p class="font-size-12">' . $tabVaule[3]["tabContent"] . '</p>';
-              $output .= '</div>';
-            $output .= '</div>';
-            $output .= '<div class="col-md-2">';
-              $output .= '<div class="text-align-center">';
-                $output .= '<p>' . $tabVaule[4]["tabNumber"] . '</p>';
-                $output .= '<p class="font-size-12">' . $tabVaule[4]["tabContent"] . '</p>';
-              $output .= '</div>';
-            $output .= '</div>';
-
+            }
           $output .= '</div>';
         $output .= '</div>';
         $output .= '<div id="menu1" class="tab-pane fade">';
