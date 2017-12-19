3177a6b5222cc1b313a875b26a22de3f95d5ec61
MengqiqiJo <763786730@qq.com>
2017-12-19 09:26:52 -0500

    extract repeat code

--- bootstrapPopUp/phizerModalAndTab.php
+++ bootstrapPopUp/phizerModalAndTab.php
@@ -26,31 +26,33 @@
               $output .= '<div class="modal-header">';
                 $output .= '<button type="button" class="close" data-dismiss="modal">&times;</button>';
                 $output .= '<h4 class="modal-title">Modal Header</h4>';
-                $output .= '<ul class="nav nav-tabs">';
-                  $output .= '<li class="active"><a data-toggle="tab" href="#home">Home</a></li>';
-                  $output .= '<li><a data-toggle="tab" href="#menu1">Menu 1</a></li>';
-                  $output .= '<li><a data-toggle="tab" href="#menu2">Menu 2</a></li>';
-                  $output .= '<li><a data-toggle="tab" href="#menu3">Menu 3</a></li>';
-                $output .= '</ul>';
 
-                $output .= '<div class="tab-content">';
-                  $output .= '<div id="home" class="tab-pane fade in active">';
-                    $output .= '<h3>HOME</h3>';
-                    $output .= '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>';
-                  $output .= '</div>';
-                  $output .= '<div id="menu1" class="tab-pane fade">';
-                    $output .= '<h3>Menu 1</h3>';
-                    $output .= '<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>';
-                  $output .= '</div>';
-                  $output .= '<div id="menu2" class="tab-pane fade">';
-                    $output .= '<h3>Menu 2</h3>';
-                    $output .= '<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>';
-                  $output .= '</div>';
-                  $output .= '<div id="menu3" class="tab-pane fade">';
-                    $output .= '<h3>Menu 3</h3>';
-                    $output .= '<p>Eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>';
-                  $output .= '</div>';
-                $output .= '</div>';
+                // $output .= '<ul class="nav nav-tabs">';
+                //   $output .= '<li class="active"><a data-toggle="tab" href="#home">Home</a></li>';
+                //   $output .= '<li><a data-toggle="tab" href="#menu1">Menu 1</a></li>';
+                //   $output .= '<li><a data-toggle="tab" href="#menu2">Menu 2</a></li>';
+                //   $output .= '<li><a data-toggle="tab" href="#menu3">Menu 3</a></li>';
+                // $output .= '</ul>';
+
+                // $output .= '<div class="tab-content">';
+                //   $output .= '<div id="home" class="tab-pane fade in active">';
+                //     $output .= '<h3>HOME</h3>';
+                //     $output .= '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>';
+                //   $output .= '</div>';
+                //   $output .= '<div id="menu1" class="tab-pane fade">';
+                //     $output .= '<h3>Menu 1</h3>';
+                //     $output .= '<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>';
+                //   $output .= '</div>';
+                //   $output .= '<div id="menu2" class="tab-pane fade">';
+                //     $output .= '<h3>Menu 2</h3>';
+                //     $output .= '<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>';
+                //   $output .= '</div>';
+                //   $output .= '<div id="menu3" class="tab-pane fade">';
+                //     $output .= '<h3>Menu 3</h3>';
+                //     $output .= '<p>Eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>';
+                //   $output .= '</div>';
+                // $output .= '</div>';
+                $output .= $this->getTabContent();
               $output .= '</div>';
 
               $output .= '<div class="modal-body">';
@@ -84,15 +86,38 @@
         $output .= '</div>';
       $output .= '</div>';
 
-
-
-
       return $output;
     }
 
     public function getTabContent() {
       $output = '';
+      $output .= '<ul class="nav nav-tabs">';
+        $output .= '<li class="active"><a data-toggle="tab" href="#home">Home</a></li>';
+        $output .= '<li><a data-toggle="tab" href="#menu1">Menu 1</a></li>';
+        $output .= '<li><a data-toggle="tab" href="#menu2">Menu 2</a></li>';
+        $output .= '<li><a data-toggle="tab" href="#menu3">Menu 3</a></li>';
+      $output .= '</ul>';
+
+      $output .= '<div class="tab-content">';
+        $output .= '<div id="home" class="tab-pane fade in active">';
+          $output .= '<h3>HOME</h3>';
+          $output .= '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>';
+        $output .= '</div>';
+        $output .= '<div id="menu1" class="tab-pane fade">';
+          $output .= '<h3>Menu 1</h3>';
+          $output .= '<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>';
+        $output .= '</div>';
+        $output .= '<div id="menu2" class="tab-pane fade">';
+          $output .= '<h3>Menu 2</h3>';
+          $output .= '<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>';
+        $output .= '</div>';
+        $output .= '<div id="menu3" class="tab-pane fade">';
+          $output .= '<h3>Menu 3</h3>';
+          $output .= '<p>Eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>';
+        $output .= '</div>';
+      $output .= '</div>';
       return $output;
+
     }
 
   }
