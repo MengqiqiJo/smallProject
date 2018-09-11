2e5a313e997d93100e07b3cde23d7b785009a40c
MengqiqiJo <763786730@qq.com>
2018-09-11 11:10:35 -0400

    install style

--- webpack-demo/src/js/app.js
+++ webpack-demo/src/js/app.js
@@ -1,8 +1,10 @@
+import '../css/style.css';
+
 function component() {
   let element = document.createElement('div');
 
   // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack1'], ' ');
+  element.innerHTML = _.join(['Hello', 'webpack12'], ' ');
 
   return element;
 }
