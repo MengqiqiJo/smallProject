281abb5472d2b1bac6f1fcdb7354ee0326f70619
MengqiqiJo <763786730@qq.com>
2018-11-16 10:28:21 -0500

    dev bootstrap modal

--- my-bootstrap-modal/index.js
+++
@@ -1,6 +0,0 @@
-// Fill modal with content from link href
-// $("#myModal").on("show.bs.modal", function(e) {
-//     var link = $(e.relatedTarget).data('load-url');
-//     $(this).find(".modal-body").load(link);
-// });
-
