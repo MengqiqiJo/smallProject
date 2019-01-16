b65c0e2196027c22a0ec978cb36ea588d0cbfe8f
MengqiqiJo <763786730@qq.com>
2019-01-16 12:52:11 -0500

    create a new angular 6 and d3 project

---
+++ angular6practice/src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component,  AfterContentInit} from '@angular/core';
+import * as d3 from 'd3';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements AfterContentInit {
+  title = 'd3 app';
+
+  radius = 10;
+
+  ngAfterContentInit() {
+    d3.select('p').style('color', 'red');
+  }
+
+  colorMe() {
+    d3.select('button').style('color', 'red');
+  }
+
+  clicked(event: any) {
+    d3.select(event.target).append('circle')
+      .attr('cx', event.x)
+      .attr('cy', event.y)
+      .attr('r', () => {
+        return this.radius;
+      })
+      .attr('fill', 'red');
+  }
+}
