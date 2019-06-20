2b2c608403242cea398a53d44d91ba9cc4dfa0a1
xinyi zhou <763786730@qq.com>
2019-06-20 11:01:38 -0400

    delete useless code

--- angular7-primeng7-template/src/app/app.component.ts
+++ angular7-primeng7-template/src/app/app.component.ts
@@ -81,44 +81,6 @@ export class AppComponent implements OnInit {
 
   }
 
-  // updateTherapeuticAreas(event) {
-  //   var temporaryTherap = [];
-
-  //   this.therapArea.forEach(thearpData => {
-  //     if (thearpData.businessUnitId == event.value.businessUnitId) {
-  //       console.log("thearpData");
-  //       console.log(thearpData);
-  //       // this.availableTherapArea.push(thearpData);
-  //       // console.log(this.availableTherapArea);
-  //       temporaryTherap.push(thearpData);
-  //     }
-  //   });
-
-  //   this.availableTherapArea = [];
-  //   this.availableTherapArea = temporaryTherap;
-  //   this.availableProgram = [];
-  // }
-
-  // updateProgram(event) {
-  //   var temopraryProgram = [];
-
-  //   this.programArea.forEach(programData => {
-  //     if (programData.therapAreaId == event.value.therapAreaId) {
-  //       temopraryProgram.push(programData);
-  //       console.log(temopraryProgram);
-  //     }
-  //   });
-
-  //   this.availableProgram = [];
-  //   this.availableProgram = temopraryProgram;
-  // }
-
-  // updateSubmitButton(event) {
-  //   console.log(event);
-  //   console.log(this.enableHCPSubmitButton);
-  //   this.enableHCPSubmitButton = false;
-  //   console.log(this.enableHCPSubmitButton);
-  // }
 
   showDialog() {
     this.display = true;
