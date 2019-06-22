1fb1f264242a4250b82ea8e75f1bed202fa5d374
xinyi zhou <763786730@qq.com>
2019-06-21 20:34:13 -0400

    dev angular7 style

--- angular7-primeng7-template/src/app/primeng-table/primeng-table.component.ts
+++ angular7-primeng7-template/src/app/primeng-table/primeng-table.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-primeng-table',
   templateUrl: './primeng-table.component.html',
-  styleUrls: ['./primeng-table.component.css']
+  styleUrls: ['./primeng-table.component.css'],
+  encapsulation: ViewEncapsulation.None
+
 })
 export class PrimengTableComponent implements OnInit {
 
