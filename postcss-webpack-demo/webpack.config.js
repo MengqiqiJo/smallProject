dabd1f65bad7fa6e1ae878773d3861439612da93
xinyi zhou <763786730@qq.com>
2020-03-19 02:41:37 -0400

    add postcss to webpack

--- postcss-webpack-demo/webpack.config.js
+++ postcss-webpack-demo/webpack.config.js
@@ -1,29 +1,36 @@
 // webpack v4
-const path = require('path');// update from 23.12.2018
+const path = require('path');
 const nodeExternals = require('webpack-node-externals');const ExtractTextPlugin = require('extract-text-webpack-plugin');module.exports = {
   entry: { main: './src/index.js' },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'postdemo.js'
   },
-  target: 'node', // update from 23.12.2018
-  externals: [nodeExternals()], // update from 23.12.2018
+  target: 'node',
+  externals: [nodeExternals()],
   module: {
     rules: [
       {
         test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
+        exclude: /node_modules/
       },
       {
-       test: /\.css$/,
-      exclude: /node_modules/,
-       use: [
-         'style-loader',
-         'css-loader',
-       ],
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+            }
+          },
+          {
+            loader: 'postcss-loader'
+          }
+        ]
       }
     ]
   }
